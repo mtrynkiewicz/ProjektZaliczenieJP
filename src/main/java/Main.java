96314aa48e58f9a4be358325a97f9a8762135c97
Mateusz Trynkiewicz <mateusz.trynkiewicz@outlook.com>
2019-06-25 22:08:21 +0100

    Konfiguracje

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.sql.Time;
+import java.util.TimeZone;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        TimeZone time_zone_default
+                = TimeZone.getDefault();
+
+        // Displaying the default TimeZone
+        System.out.println("Default TimeZone: "
+                + time_zone_default);
+        int z =3;
+    }
+}
